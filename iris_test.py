9f70cbf6e8ce47eb2f9f1f9954040e15a62639ce
chihiro1206 <neodymium1206@gmail.com>
2025-07-13 22:37:32 +0900

    決定木の練習

--- iris_test.py
+++ iris_test.py
@@ -1,88 +1,48 @@
-"""from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn import datasets
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
-#必要なライブラリをインポートしてる
-
-from sklearn import datasets
-
-digits = datasets.load_digits()
-
-# 特徴量データ（説明変数）を表示
-print(digits.data)
-
-# 正解ラベルを表示
-print(digits.target)
-
-# データの形状（サンプル数×特徴量数）
-print(digits.data.shape)
-
-
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-from sklearn import datasets
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
-
-iris = datasets.load_iris()
-
-print(iris.keys())
-#irisのデータセットのキーを表示
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-from sklearn import datasets
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
-
-iris = datasets.load_iris()
-
-# Load as pandas.DataFrame
-df_iris = pd.DataFrame(data=iris.data, columns=iris.feature_names)
-df_iris['target'] = iris.target
-print(df_iris.describe())
-data_train, data_test, target_train, target_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=0)"""
-
-
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.tree import plot_tree
 from sklearn.datasets import load_iris
+from sklearn.neural_network import MLPClassifier
 import matplotlib.pyplot as plt
-# アヤメのデータを読み込む
+
 iris = load_iris()
 X = iris.data       # 特徴（花の長さなど）
 y = iris.target     # 答え（花の種類）
 
 # データを「学習用」と「テスト用」に分ける
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
-# 決定木（はい・いいえで分ける木）のモデルを作る
+# 決定木モデルを作る
 model = DecisionTreeClassifier()
 
-# 学習用データで学習させる（覚えさせる）
+# 学習用データで学習
 model.fit(X_train, y_train)
 
 # テストデータを使って予想させる
 y_pred = model.predict(X_test)
 
-print(iris.feature_names)
-print(iris.target_names)
-"""iris = load_iris()
-model = DecisionTreeClassifier(max_depth=3, random_state=0)
-model.fit(iris.data, iris.target)
+"""# Split the dataset
+data_train, data_test, target_train, target_test = train_test_split(
+    iris.data, iris.target, test_size=0.5, random_state=0)
+
+# Define Neural Neowork model
+clf = MLPClassifier(hidden_layer_sizes=10, activation='relu',
+                    solver='adam', max_iter=1000)
+
+# Lerning model
+clf.fit(data_train, target_train)
+
+
+# Show loss curve
+plt.plot(clf.loss_curve_)
+plt.title("Loss Curve")
+plt.xlabel("Iteration")
+plt.ylabel("Loss")
+plt.grid()
+plt.show()"""
+
 
 # 決定木を図にする
 plt.figure(figsize=(14, 8))
@@ -93,9 +53,9 @@ plot_tree(
     class_names=iris.target_names,
     rounded=True
 )
-plt.show()"""
+plt.show()
 
-"""# 正解率を計算
+# 正解率を計算
 accuracy = accuracy_score(y_test, y_pred)
 print(f"正解率: {accuracy * 100:.2f}%")
 
@@ -104,4 +64,4 @@ my_flower = [[5.0, 3.6, 1.4, 0.2]]  # Setosaっぽいデータ
 
 # 予想させる
 prediction = model.predict(my_flower)
-print("予想された花の種類:", iris.target_names[prediction[0]])"""
+print("予想された花の種類:", iris.target_names[prediction[0]])
