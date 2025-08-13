71910f2609a4bb3ca89ef0041b6f9b0dd14e8319
chihiro1206 <neodymium1206@gmail.com>
2025-08-13 15:48:27 +0900

    実験1回目（失敗）

--- iris_test.py
+++ iris_test.py
@@ -12,7 +12,7 @@ X = iris.data       # 特徴（花の長さなど）
 y = iris.target     # 答え（花の種類）
 
 # データを「学習用」と「テスト用」に分ける
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.9)
 
 # 決定木モデルを作る
 model = DecisionTreeClassifier()
@@ -23,45 +23,8 @@ model.fit(X_train, y_train)
 # テストデータを使って予想させる
 y_pred = model.predict(X_test)
 
-"""# Split the dataset
-data_train, data_test, target_train, target_test = train_test_split(
-    iris.data, iris.target, test_size=0.5, random_state=0)
-
-# Define Neural Neowork model
-clf = MLPClassifier(hidden_layer_sizes=10, activation='relu',
-                    solver='adam', max_iter=1000)
-
-# Lerning model
-clf.fit(data_train, target_train)
-
-
-# Show loss curve
-plt.plot(clf.loss_curve_)
-plt.title("Loss Curve")
-plt.xlabel("Iteration")
-plt.ylabel("Loss")
-plt.grid()
-plt.show()"""
-
-
-# 決定木を図にする
-plt.figure(figsize=(14, 8))
-plot_tree(
-    model,
-    filled=True,
-    feature_names=iris.feature_names,
-    class_names=iris.target_names,
-    rounded=True
-)
-plt.show()
 
 # 正解率を計算
 accuracy = accuracy_score(y_test, y_pred)
-print(f"正解率: {accuracy * 100:.2f}%")
+print(f"学習量10%正解率: {accuracy * 100:.2f}%")
 
-# 自分で花の情報を入れてみる（例）
-my_flower = [[5.0, 3.6, 1.4, 0.2]]  # Setosaっぽいデータ
-
-# 予想させる
-prediction = model.predict(my_flower)
-print("予想された花の種類:", iris.target_names[prediction[0]])
