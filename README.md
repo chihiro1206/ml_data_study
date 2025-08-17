a66909f3e5e9be1f09a61e8e4d1a7ff23b5e6080
chihiro1206 <neodymium1206@gmail.com>
2025-08-17 20:14:47 +0900

    changed the text in README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ml_data_study
 Code for a research project on training data in machine learning. Planning to explore how different amounts of data affect model results.
+機械学習のプログラムは学習するデータの質や量によって精度がどう変化するかを調べています。
+今後は決定木だけでなく複数のモデルで試してみたり、画像の情報などデータの形も変えて実験したいと考えています。
+This code is being developed by Chihiro Kobayashi.
+
--- iris_test.py
+++ iris_test.py
@@ -9,20 +9,17 @@ X = iris.data
 y = iris.target
 
 # テストデータの固定　ここで学習データとテストは分離する
-X_train_full, X_test, y_train_full, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42, stratify=y
-)
+X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
 
 # 上で分離した学習データの中からさらに学習に使う割合を指定する
-train_frac = 0.6  
-
+train_frac = 0.1  # 100%のデータを学習に使う
 
 sss = StratifiedShuffleSplit(n_splits=1, train_size=train_frac, random_state=0)
 for train_idx, _ in sss.split(X_train_full, y_train_full):
     X_train = X_train_full[train_idx]
     y_train = y_train_full[train_idx]
 
-# モデル学習
+# モデル学習2
 model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
 
