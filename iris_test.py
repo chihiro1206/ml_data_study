fcd9ab2c8308dafd4a0ff5d330869a4f3f276d6e
chihiro1206 <neodymium1206@gmail.com>
2025-08-18 21:41:13 +0900

    Update iris_test.py
    コメントの変更

--- iris_test.py
+++ iris_test.py
@@ -6,22 +6,22 @@ from sklearn.tree import plot_tree
 
 
 iris = load_iris()
-X = iris.data       # 特徴（花の長さなど）
-y = iris.target     # 答え（花の種類）
+X = iris.data       
+y = iris.target     
 
 # データを「学習用」と「テスト用」に分ける
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.9)
 
-# 決定木モデルを作る
+# 決定木モデル
 model = DecisionTreeClassifier()
 
-# 学習用データで学習
+# 学習用データを学習
 model.fit(X_train, y_train)
 
-# テストデータを使って予想させる
+# テストデータを使って予想
 y_pred = model.predict(X_test)
 
-# 正解率を計算
+# 正解率
 accuracy = accuracy_score(y_test, y_pred)
 print(f"学習量10%正解率: {accuracy * 100:.2f}%")
 
