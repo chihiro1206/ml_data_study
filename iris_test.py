8c0184b14e1646bee7dafd1216193cb64b499d6f
chihiro1206 <neodymium1206@gmail.com>
2025-09-21 12:00:18 +0900

    use wine dataset

--- iris_test.py
+++ iris_test.py
@@ -1,25 +1,24 @@
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-
-
-iris = load_iris()
-X = iris.data
-y = iris.target
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+wine = load_wine()
+X = wine.data
+y = wine.target
 
 # テストデータの固定　ここで学習データとテストは分離する
 X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
 
 # 上で分離した学習データの中からさらに学習に使う割合を指定する
-train_frac = 0.1  # 100%のデータを学習に使う
+train_frac = 1
 
-sss = StratifiedShuffleSplit(n_splits=1, train_size=train_frac, random_state=0)
-for train_idx, _ in sss.split(X_train_full, y_train_full):
-    X_train = X_train_full[train_idx]
-    y_train = y_train_full[train_idx]
-
-# モデル学習2
+"""sss = StratifiedShuffleSplit(n_splits=1, train_size=train_frac, random_state=0)
+for train_idx, _ in sss.split(X_train_full, y_train_full):"""
+X_train = X_train_full
+y_train = y_train_full
+#[train_idx]
 model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
 
@@ -28,3 +27,7 @@ y_pred = model.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
 
 print(f"学習割合{int(train_frac*100)}%のとき正解率: {acc*100:.2f}%")
+
+"""plt.figure(figsize=(12,8))
+plot_tree(model,filled=True,feature_names=wine.feature_names,class_names=wine.target_names)
+plt.show()"""
